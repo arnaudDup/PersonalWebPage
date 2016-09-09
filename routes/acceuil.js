340a646be1ba7162af48ceaeb76a7d73e62b6586
Arnaud <arnaud.dupeyrat@gmail.com>
2016-09-09 23:09:43 +0200

    remavo cap

--- routes/acceuil.js
+++ routes/acceuil.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 // on affiche la page d'acceuil.
-  res.sendFile('Acceuil.html', {root: __dirname + './../public'});
+  res.sendFile('acceuil.html', {root: __dirname + './../public'});
 });
 
 module.exports = router;
